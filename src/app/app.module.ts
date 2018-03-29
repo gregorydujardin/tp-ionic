c91014ce468eca5e39d2340d4d59501ca0f30c07
ben <ben@yopmail.com>
2018-03-29 13:32:04 +1100

    organize tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { ListePage } from '../pages/liste/liste';
+import { CartePage } from '../pages/carte/carte';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +13,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    ListePage,
+    CartePage,
     TabsPage
   ],
   imports: [
@@ -26,9 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    ListePage,
+    CartePage,
     TabsPage
   ],
   providers: [
